cc0726d125a2ae4bc2db85740873715f2460dd80
Tariq Yaqub <ehstaya@gmail.com>
2018-11-17 11:45:11 -0800

    Initial commit

---
+++ src/main/java/com/india/edu/school/SchoolappApplication.java
@@ -0,0 +1,17 @@
+package com.india.edu.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/*
+ * this is start
+ */
+
+@SpringBootApplication
+public class SchoolappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolappApplication.class, args);
+	}
+}
