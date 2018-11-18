873bdf738d4e3b176e0f8f7dedf62056524e5af8
Tariq Yaqub <ehstaya@gmail.com>
2018-11-17 16:47:24 -0800

    added

--- src/main/java/com/india/edu/school/SchoolappApplication.java
+++ src/main/java/com/india/edu/school/SchoolappApplication.java
@@ -1,7 +1,18 @@
 package com.india.edu.school;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import javax.sql.DataSource;
+
 
 
 /*
@@ -10,8 +21,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SchoolappApplication {
+	
+	 @Autowired
+	    private Environment env;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolappApplication.class, args);
 	}
+	
+	@Bean(name = "dataSource")
+    public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+ 
+        // See: application.properties
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        dataSource.setUrl(env.getProperty("spring.datasource.url"));
+        dataSource.setUsername(env.getProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.password"));
+ 
+        System.out.println("## getDataSource: " + dataSource);
+ 
+        return dataSource;
+    }
 }
